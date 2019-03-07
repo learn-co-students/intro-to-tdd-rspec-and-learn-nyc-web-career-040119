d2b628018e75a1ed800b6e30728476d4cbd60e94
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-07 03:56:26 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -5,11 +5,5 @@ require_relative '../current_age_for_birth_year.rb'
     age_of_person = current_age_for_birth_year(1984)
 
     expect(age_of_person).to eq(19)
-    
-    def 
-      current_age_for_birth_year
-      (birth_year)
-      2003 - birth_year
     end
-      end
 end
